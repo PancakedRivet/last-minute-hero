70408d1a9c69beb48da6cbdc88caa5610529bf2d
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2026-01-08 21:12:44 +1300

    Formatting

--- scripts/ui.lua
+++ scripts/ui.lua
@@ -1,9 +1,9 @@
 -- The main UI with health, coins and score
 function draw_ui(_player, _game_sprites)
-    rectfill(0, 0, 128, 9, 6)
     -- UI Background
+    rectfill(0, 0, 128, 9, 6)
+    -- Health Bar UI
     draw_status_bar(_player.health, _player.max_health, 9, 2, 77, 3)
-    -- Health sprite in top left corner
     spr(_game_sprites.health, 1, 1)
     -- Coin UI
     spr(_game_sprites.coin, 90, 1)
@@ -18,8 +18,8 @@ function draw_status_bar(_current_value, _max_value, _x, _y, _width, _height)
     local filled_width = flr(_width * bar_ratio)
     local border_width = 1
 
-    local bar_color = 11
     -- Green by default
+    local bar_color = 11
     if bar_ratio < 0.5 then
         bar_color = 10 -- Yellow
     end
@@ -27,10 +27,10 @@ function draw_status_bar(_current_value, _max_value, _x, _y, _width, _height)
         bar_color = 8 -- Red
     end
 
-    rectfill(_x, _y, _x + _width + (2 * border_width), _y + _height + (2 * border_width), 7)
     -- Background Edge
-    rectfill(_x + border_width, _y + border_width, _x + _width + border_width, _y + _height + border_width, 0)
+    rectfill(_x, _y, _x + _width + (2 * border_width), _y + _height + (2 * border_width), 7)
     -- Background
-    rectfill(_x + border_width, _y + border_width, _x + filled_width + border_width, _y + _height + border_width, bar_color)
+    rectfill(_x + border_width, _y + border_width, _x + _width + border_width, _y + _height + border_width, 0)
     -- Filled part
+    rectfill(_x + border_width, _y + border_width, _x + filled_width + border_width, _y + _height + border_width, bar_color)
 end
