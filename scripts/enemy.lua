4318a0fbacac0db46643f0e4401cdc6c19b6fb55
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2026-01-26 22:17:09 +1300

    Make better use of attack range for enemy movement

--- scripts/enemy.lua
+++ scripts/enemy.lua
@@ -3,22 +3,23 @@ enemy_animation_frames_per_sprite = 8
 
 function update_enemies()
     if count(enemies) < max_enemies then
-        -- Spawn a new enemy
+        -- spawn a new enemy
         add(enemies, new_enemy())
     end
-    -- Move enemies towards player
+    -- move enemies towards player
     for enemy in all(enemies) do
-        if enemy.x < player.x then
-            enemy.x += enemy.speed
-        elseif enemy.x > player.x then
-            enemy.x -= enemy.speed
+        -- check horizontal movement
+        local dx = abs(player.x - enemy.x)
+        local dy = abs(player.y - enemy.y)
+        local x_dir = sgn(player.x - enemy.x)
+        local y_dir = sgn(player.y - enemy.y)
+        if dx > enemy.attack_range_x then
+            enemy.x += enemy.speed * x_dir
         end
-        if enemy.y < player.y then
-            enemy.y += enemy.speed
-        elseif enemy.y > player.y then
-            enemy.y -= enemy.speed
+        if dy > enemy.attack_range_y then
+            enemy.y += enemy.speed * y_dir
         end
-        -- Update enemy animations
+        -- update enemy animations
         update_animation_walk(enemy, #enemy_animation_sprites_walk, enemy_animation_frames_per_sprite)
     end
 end
@@ -32,6 +33,8 @@ function new_enemy()
         max_health = 20,
         speed = 1,
         attack = 1,
+        attack_range_x = 6,
+        attack_range_y = 0,
         attacked = false,
         animation_idx = 1,
         animation_tick = 0
@@ -61,7 +64,7 @@ function draw_enemies(_enemies)
     for enemy in all(_enemies) do
         spr(enemy_animation_sprites_walk[enemy.animation_idx], enemy.x, enemy.y)
     end
-    -- Draw the health bars after frawing the sprites so that they appear on top
+    -- draw the health bars after drawing the sprites so that they appear on top
     for enemy in all(_enemies) do
         draw_status_bar(enemy.health, enemy.max_health, enemy.x - 3, enemy.y - 5, enemy.x + 8, enemy.y - 4)
     end
